38198506cc73b5c5b49a3261a24c60cf46e54a2e
Joe Curtin <42@jbcurtin.io>
2020-09-28 21:32:24 -0400

    job_name error

--- .circleci/builder/factory.py
+++ .circleci/builder/factory.py
@@ -267,8 +267,8 @@ def main(options: argparse.Namespace) -> None:
         deploy_job_name = 'Deploy JDAT Notebooks'
         deploy_job = copy.deepcopy(deploy_website_job)
         config['jobs'][deploy_job_name] = deploy_job
-        config['workflows']['Deploy Website']['jobs'].append(deploy_job_name)
-        # config['workflows']['Deploy Website']['jobs'].append({deploy_job_name: {'requires': [k for k in config['jobs'].keys()]}})
+        # config['workflows']['Branch Build']['jobs'].append(deploy_job_name)
+        config['workflows']['Branch Build']['jobs'].append({deploy_job_name: {'requires': [k for k in config['jobs'].keys()]}})
 
         with open(CIRCLE_CI_CONFIG_PATH, 'wb') as stream:
             stream.write(yaml.dump(config).encode('utf-8'))
