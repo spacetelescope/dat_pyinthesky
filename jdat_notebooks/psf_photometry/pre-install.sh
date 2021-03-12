07f09f1042b7b509e21bec4a02a4a72a6c92b213
Robel Geda <rgeda@stsci.edu>
2021-03-12 03:23:19 -0500

    psf_photometry/pre-install.sh

--- jdat_notebooks/psf_photometry/pre-install.sh
+++ jdat_notebooks/psf_photometry/pre-install.sh
@@ -15,9 +15,21 @@ if [ ! -f "$filepath" ]; then
     cd -
 fi
 
-wget -qO- https://ssb.stsci.edu/trds/tarfiles/synphot1.tar.gz | tar xvz
-wget -qO- https://ssb.stsci.edu/trds/tarfiles/synphot2.tar.gz | tar xvz
-wget -qO- https://ssb.stsci.edu/trds/tarfiles/synphot5.tar.gz | tar xvz
-wget -qO- https://ssb.stsci.edu/trds/tarfiles/synphot6.tar.gz | tar xvz
+
+if [ ! -f "grp" ]; then
+
+    curl -v https://ssb.stsci.edu/trds/tarfiles/synphot1.tar.gz -o synphot1.tar.gz | tar xvz
+    tar -xzf synphot1.tar.gz
+
+    curl -v https://ssb.stsci.edu/trds/tarfiles/synphot2.tar.gz -o synphot2.tar.gz | tar xvz
+    tar -xzf synphot2.tar.gz
+
+    curl -v https://ssb.stsci.edu/trds/tarfiles/synphot5.tar.gz -o synphot5.tar.gz | tar xvz
+    tar -xzf synphot5.tar.gz
+
+    curl -v https://ssb.stsci.edu/trds/tarfiles/synphot6.tar.gz -o synphot6.tar.gz | tar xvz
+    tar -xzf synphot6.tar.gz
+
+fi
 
 exit 0
