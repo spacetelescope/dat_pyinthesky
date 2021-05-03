8f5cabf86f63059103fe3640b71f90bfc3a886b9
Michael Gough <gough@stsci.edu>
2021-05-03 13:18:33 -0400

    regenerated notebook build machinery

--- .circleci/setup-env.sh
+++ .circleci/setup-env.sh
@@ -10,8 +10,8 @@ apt-get install -y git make build-essential libssl-dev zlib1g-dev libbz2-dev lib
     llvm libncurses5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev libfreetype6-dev
 
 git clone https://github.com/spacetelescope/nbcollection nbcollection
-git checkout fab2bce05cd24010a6c0f94e76bdfb83e9999096
 cd nbcollection
+git checkout fab2bce05cd24010a6c0f94e76bdfb83e9999096 
 pip install -U pip setuptools
 pip install -r ci_requirements.txt
 python setup.py install
