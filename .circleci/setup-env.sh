635870b4c835887f736837887e998581409ddd60
Michael Gough <gough@stsci.edu>
2021-10-26 10:58:03 -0400

    Updates for MRS notebooks

--- .circleci/setup-env.sh
+++ .circleci/setup-env.sh
@@ -4,8 +4,8 @@ set -e
 
 export LANG=C.UTF-8
 export LC_ALL=C.UTF-8
-conda install python=3.7.11
-#conda install python=3.8.10
+#conda install python=3.7.11
+conda install python=3.8.10
 apt-get update
 apt-get install -y git make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl \
     llvm libncurses5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev libfreetype6-dev
@@ -15,7 +15,6 @@ cd nbcollection
 pip install -U pip setuptools
 pip install -r ci_requirements.txt
 python setup.py install
-pip install numpy
-export CRDS_SERVER_URL=https://jwst-crds.stsci.edu
-export CRDS_PATH=$HOME/crds_cache
+#export CRDS_SERVER_URL=https://jwst-crds.stsci.edu
+#export CRDS_PATH=$HOME/crds_cache
 cd -
