5b74d278e871a3fd94ad91fe60b3670c452c5918
Ori <ofox@stsci.edu>
2022-06-30 14:58:13 -0400

    added real MIRI data

---
+++ jdat_notebooks/MIRI_PSF_Photometry/webbpsf_install.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -e
+
+mkdir -p webbpsf-data
+FILE_URL='https://stsci.box.com/s/slmgdud1vz9mmbucbsl1bo4dzm9ft4t3'
+filename=$(basename $FILE_URL)
+
+if [ ! -f "$filepath" ]; then
+    wget https://stsci.box.com/shared/static/34o0keicz2iujyilg4uz617va46ks6u9.gz --no-check-certificate
+    tar -xf 34o0keicz2iujyilg4uz617va46ks6u9.gz
+    export WEBBPSF_PATH=./webbpsf-data
+fi
+exit 0
